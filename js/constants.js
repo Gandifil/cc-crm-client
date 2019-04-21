789e0970fc82e408e78ef5b78ab32131badb0030
Gandifil <Gandifilmk@gmail.com>
2019-04-21 18:26:10 +0400

    Add table of indactions

--- js/constants.js
+++ js/constants.js
@@ -3,7 +3,4 @@ const unlogined = 0;
 const loginedByUser = 1;
 const loginedByAdmin = 2;
 
-// server endpoint
-const server = 'http://localhost';
-
-// 
+//
--- js/vue-app.js
+++ js/vue-app.js
@@ -3,80 +3,66 @@ window.app = new Vue({
     el: "#app",
     data:
         {
+            searchQuery: '',
+            gridColumns: [],
+            gridData: [],
+
             state: unlogined,
             currentWindow: 'signIn',
             token: '',
         },
     methods: {
         btnRegist: function (event) {
-            username = document.getElementById("reg_login").value;
-            password = document.getElementById("reg_password").value;
-
-            axios.post(server + '/auth/register',
-                {
-                    username: username,
-                    password: password,
-                },
-                {
-                    headers:
-                        {
-                            'Content-Type': 'application/json'
-                        }
-                }
-            )
-                .then(response => {})
-                .catch(e => {
-                    console.error(e.message);
-                });
+            requestProxy('/auth/register', {
+                username: document.getElementById("reg_login").value,
+                password: document.getElementById("reg_password").value,
+                phoneNumber: '231312314',
+            },response => { alert('удачно');}, x => {alert('ошибка'); });
         },
-        btnLogin: function (event) {
-            username = document.getElementById("login").value;
-            password = document.getElementById("password").value;
 
-            axios.post(server + '/auth/login',
-                {
-                    username: username,
-                    password: password,
-                },
-                {
-                    headers:
-                        {
-                            'Content-Type': 'application/json'
-                        }
-                }
-            )
-                .then(response => {
+        btnLogin: function (event) {
+            requestProxy('/auth/login',{
+                    username: document.getElementById("login").value,
+                    password: document.getElementById("password").value,
+                }, response => {
                     this.token = response.data.token;
                     this.state = loginedByUser;
                     this.currentWindow = 'setIndaction';
-                })
-                .catch(e => {
+                },e => {
+                    alert('ошибка');
                     console.error(e.message);
-                });
-        },
-        btnSetIndaction: function () {
-            hotWater = document.getElementById("hotWater").value;
-            coldWater = document.getElementById("coldWater").value;
-            light = document.getElementById("light").value;
-
-            axios.post(server + '/meters/send',
-                {
-                    hot_water: hotWater,
-                    cold_water: coldWater,
-                    electricity: light,
                 },
+                this.token
+            );
+        },
+
+        btnSetIndaction: function () {
+            requestProxy( '/meters/send',
                 {
-                    headers:
-                        {
-                            'Content-Type': 'application/json',
-                            'Authorization': "Bearer " + this.token,
-                        }
-                }
-            )
-                .then(response => {
-                })
-                .catch(e => {
+                    hotWater: document.getElementById("hotWater").value,
+                    coldWater: document.getElementById("coldWater").value,
+                    electricity: document.getElementById("light").value,
+                },response => {}, e => {
+                    alert('ошибка');
                     console.error(e.message);
-                });
+                },
+                this.token
+            );
+        },
+
+        btnShowIndaction: function(){
+            requestGetProxy( '/meters/?periodType=ALL',
+                response => {
+                    let grid = this.$refs.indications_grid;
+                    grid.heroes = response.data;
+                    grid.columns = ['electricity', 'hotWater', 'coldWater', 'date'];
+                    grid.$forceUpdate();
+                    //console.log(gridData);
+                }, e => {
+                    alert('ошибка');
+                },
+                this.token
+            );
         }
-    }});
+    }
+});
